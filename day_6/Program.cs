a8f74c8faf786efb5c8cbe707a3eef79716ef6b2
LigaKlavina <ligaiklavinai@gmail.com>
2018-05-11 19:47:30 +0300

    day_6
    commit

---
+++ day_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace day_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ievadiet skaitli:");
+
+            string ievads = Console.ReadLine();
+
+            Console.WriteLine(Pievienoskaitli(ievads));
+
+            string izvade = Pievienoskaitli(ievads);
+
+            Console.WriteLine(izvade);
+
+            Console.ReadLine();
+        }
+
+        static string Pievienoskaitli(string skaitlis)
+        {
+            string rezultats = "mans skaitlis ir" + skaitlis;
+            return rezultats;
+        }
+    }
+}
